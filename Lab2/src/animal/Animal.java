53df49be561a488c854b5177769fcc2340b0561e
Marocico Gabriela <gabriela.marocico@gmail.com>
2014-11-01 18:07:59 -0700

    Update animal package

---
+++ Lab2/src/animal/Animal.java
@@ -0,0 +1,53 @@
+package animal;
+
+/**
+ * 
+ * @author gby
+ *
+ *         Aceasta este o clasa abstracta
+ */
+
+public abstract class Animal {
+	/* Numele animalului */
+	String numeAnimal;
+	boolean alive;
+	String origineContinent;
+	String hrana;
+
+	public abstract String food();
+
+	public abstract void print();
+
+	public String getNumeAnimal() {
+		return numeAnimal;
+	}
+
+	public void setNumeAnimal(String numeAnimal) {
+		this.numeAnimal = numeAnimal;
+	}
+
+	public boolean isAlive() {
+		return alive;
+	}
+
+	public void setAlive(boolean alive) {
+		this.alive = alive;
+	}
+
+	public String getOrigineContinent() {
+		return origineContinent;
+	}
+
+	public void setOrigineContinent(String origineContinent) {
+		this.origineContinent = origineContinent;
+	}
+
+	public String getHrana() {
+		return hrana;
+	}
+
+	public void setHrana(String hrana) {
+		this.hrana = hrana;
+	}
+
+}
